7ab5353331167cb3033ea419f32a252a5c125b5d
Salman Ahmed <frenzibyte@gmail.com>
2021-03-28 13:11:56 +0300

    Add triangular masking shader

---
+++ osu.Framework/Resources/Shaders/sh_Triangle.fs
@@ -0,0 +1,28 @@
+#include "sh_Utils.h"
+#include "sh_TextureWrapping.h"
+
+varying lowp vec4 v_Colour;
+varying mediump vec2 v_TexCoord;
+varying mediump vec4 v_TexRect;
+
+uniform lowp sampler2D m_Sampler;
+
+float triangleArea(vec2 v1, vec2 v2, vec2 v3)
+{
+    return abs(((v1.x)*(v2.y-v3.y)+(v2.x)*(v3.y-v1.y)+(v3.x)*(v1.y-v2.y))/2.0);
+}
+
+void main(void)
+{
+	highp vec2 v1 = vec2(0.0, v_TexRect.w+1.0);
+	highp vec2 v2 = vec2(v_TexRect.z / 2.0, 0.0);
+	highp vec2 v3 = vec2(0.0, v_TexRect.w);
+	
+	float w1 = (v1.x * (v3.y - v1.y) + ((v_TexCoord.y) - v1.y) * (v3.x - v1.x) - (v_TexCoord.x) * (v3.y - v1.y)) / ((v2.y - v1.y) * (v3.x - v1.x) - (v2.x - v1.x) * (v3.y - v1.y));
+	highp float w2 = ((v_TexCoord.y) - v1.y - w1 * (v2.y - v1.y)) / (v3.y - v1.y);
+    
+	if (w1 >= 0.0 && w2 >= 0.0 && (w1 + w2) <= 1.0)
+	    gl_FragColor = toSRGB(v_Colour * wrappedSampler(wrap(v_TexCoord, v_TexRect), v_TexRect, m_Sampler, -0.9));
+    else
+        gl_FragColor = vec4(0);
+}
